83a92f15ed269a7d5c6de763ad956d1c6a464bc8
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2023-11-09 14:52:19 -0300

    Conclusão dos endpoints do sesafio Api Bootcamp DIO

--- Program.cs
+++ Program.cs
@@ -19,12 +19,13 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{    
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
 
 app.UseAuthorization();
 
